5b0139bacc801ef0d9956b2657c65e26194cf9c4
weishaodaren <weishaodaren@gmail.com>
2021-08-20 15:19:15 +0800

    config

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+// 通用字体
+import 'vfonts/Lato.css';
+// 等宽字体
+import 'vfonts/FiraCode.css';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      Assets: resolve('src/assets'),
+      Components: resolve('src/components'),
+      Utils: resolve('src/utils'),
+      Pages: resolve('src/pages'),
+      Type: resolve('src/types'),
+    },
+  },
+  plugins: [vue()],
+});
