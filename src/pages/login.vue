23bc2bd114134a3d39053a0150313354bd34fdfc
weishaodaren <weishaodaren@gmail.com>
2021-08-23 09:08:52 +0800

    config alias

---
+++ src/pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This is Fuckin Login</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Login',
+  setup() {},
+});
+</script>
+
+<style lang="less" scoped></style>
