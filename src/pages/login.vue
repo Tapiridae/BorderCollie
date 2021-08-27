5216f9c94f4120752f90c7e4e4a85621e5e44a6e
weishaodaren <weishaodaren@gmail.com>
2021-08-27 18:07:00 +0800

    update BGC

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="login-card-container">
-    <n-card hoverable>
-      <n-space vertical size="large">
-        <n-avatar round :size="88">{{ userName }}</n-avatar>
-        <n-input
-          v-model:value="userName"
-          type="text"
-          placeholder="请输入用户名"
-          round
-        />
-        <n-input
-          v-model:value="password"
-          type="password"
-          placeholder="请输入密码"
-          show-password-toggle
-          :maxlength="16"
-          round
-        />
-        <n-button
-          round
-          type="primary"
-          ghost
-          :loading="loading"
-          @click="handleClick2Login"
-        >
-          登录
-        </n-button>
-      </n-space>
-    </n-card>
+  <div class="login-layout">
+    <div class="login-card-container">
+      <n-card hoverable>
+        <n-space vertical size="large">
+          <n-avatar round :size="88">{{ userName }}</n-avatar>
+          <n-input
+            v-model:value="userName"
+            type="text"
+            placeholder="请输入用户名"
+            round
+          />
+          <n-input
+            v-model:value="password"
+            type="password"
+            placeholder="请输入密码"
+            show-password-toggle
+            :maxlength="16"
+            round
+          />
+          <n-button
+            round
+            type="primary"
+            ghost
+            :loading="loading"
+            @click="handleClick2Login"
+          >
+            登录
+          </n-button>
+        </n-space>
+      </n-card>
+    </div>
   </div>
 </template>
 
@@ -38,14 +40,19 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
-import { defineComponent, ref } from 'vue';
-import { MessageReactive, useMessage } from 'naive-ui';
+import type { MessageReactive } from 'naive-ui';
+import { defineComponent, onMounted, ref } from 'vue';
+import { useMessage } from 'naive-ui';
 
+const color = ref('');
 const message = useMessage();
 const userName = ref('WEISHAODAREN');
 const password = ref('');
 const loading = ref(false);
 
+/**
+ * @description 点击登录
+ */
 const handleClick2Login = () => {
   if (userName.value.trim() && password.value.trim()) {
     let _message: MessageReactive;
@@ -61,12 +68,29 @@ const handleClick2Login = () => {
     message.warning('注意填写完整哦');
   }
 };
+
+onMounted(() => {
+  setInterval(() => {
+    color.value = `rgba(${parseInt(String(Math.random() * 256))},${parseInt(
+      String(Math.random() * 256)
+    )}, ${parseInt(String(Math.random() * 256))} )`;
+  }, 500);
+});
 </script>
 
 <style lang="less" scoped>
-.login-card-container {
+.login-layout {
   position: relative;
-  text-align: center;
-  width: 400px;
+  widows: 100vw;
+  height: 100vh;
+  background-color: v-bind(color);
+  .login-card-container {
+    position: absolute;
+    top: 45%;
+    left: 20%;
+    transform: translate(-25%, -45%);
+    text-align: center;
+    width: 400px;
+  }
 }
 </style>
