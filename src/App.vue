4a27fe4c0c537521c695692ba14f4c130870f3ee
weishaodaren <weishaodaren@gmail.com>
2021-09-03 14:09:05 +0800

    优化全局监听

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,33 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
-import { defineComponent, ref, provide } from 'vue';
+import { defineComponent, ref, provide, watchPostEffect } from 'vue';
 import { NConfigProvider, zhCN, dateZhCN, darkTheme } from 'naive-ui';
 import Provider from '@/provider';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 // 主题
 const theme = ref<object | null>(null);
-// 主题标记
-const themeSign = ref<boolean>(false);
+
+// 全局监听
+watchPostEffect(() => {
+  // 不在线 退出登录
+  if (!Provider.globalOnlineStatus.value) router.push('/login');
+  // 依赖缓存主题
+  if (Provider.globalThemeMode.value) theme.value = darkTheme;
+  else theme.value = null;
+});
+
 // 切换主题
 const onToggleMode = (mode: boolean) => {
-  themeSign.value = mode;
+  Provider.globalThemeMode.value = mode;
   if (mode) theme.value = darkTheme;
   else theme.value = null;
 };
 
 // 提供 切换主题方法 主题变量
 provide(Provider.ToggleThemeMode, onToggleMode);
-provide(Provider.ThemeMode, themeSign);
+provide(Provider.ThemeMode, Provider.globalThemeMode);
 </script>
--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,6 +1,23 @@
 import type { InjectionKey, Ref } from 'vue';
+import { useStorage } from '@vueuse/core';
 
+/**
+ * InjectionKey
+ */
 const ToggleThemeMode: InjectionKey<(mode: boolean) => void> = Symbol();
 const ThemeMode: InjectionKey<Ref<boolean>> = Symbol();
 
-export default { ToggleThemeMode, ThemeMode };
+/**
+ * GlobalState
+ */
+const globalThemeMode = useStorage('theme', false);
+const globalOnlineStatus = useStorage('isOnline', false);
+const globalUserName = useStorage('userName', '赵嘉伟');
+
+export default {
+  ToggleThemeMode,
+  ThemeMode,
+  globalThemeMode,
+  globalOnlineStatus,
+  globalUserName,
+};
