fe64fe126215975ab4f0568d8c5f2e9e5b34799d
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2021-11-17 02:30:05 +0000

    Deploying to release from @ Tapiridae/BorderCollie@71b9a10f1ba5761656e2956fce0b2dd2105ae14d 🚀

---
+++ assets/websocket.e8e64d46.js
@@ -0,0 +1,1 @@
+class e{static get CHAT_ROOM(){return"wss://beta-chat-ws.keyush.cn/chat_room/"}static get NOTIFICATION(){return"wss://beta-chat-ws.keyush.cn/notification"}static get UUID(){return"535fe8a3d9fb44e0bfc50ebbdb6b4b32"}}export{e as W};
